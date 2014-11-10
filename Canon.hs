ab74b91532bbb6d66c5602922422def8c5dd1562
Tom Titchener <ttitchen@verizon.net>
2014-11-10 18:03:33 -0500

    Tidy

--- Canon.hs
+++ Canon.hs
@@ -2,24 +2,38 @@
 
 module Canon where
 
+import qualified Data.ByteString.Lazy as LazyByteString
+import           Data.Ratio
 import           Score
 import           ScoreToMidi
-import           Data.Ratio
-import qualified Data.ByteString.Lazy as LazyByteString
 import qualified Sound.MIDI.File.Save as SaveFile
 
--- | First, simplest of all Canon:
---
-
+-- | First, simplest of all Canons.
+--   Imitation at unision.
 data SimpleCanon = SimpleCanon
                    {title       :: Title
                    ,notes       :: NoteMotto
                    ,distance    :: Rhythm
                    ,instrument  :: Instrument
-                   ,voices      :: Int
                    ,repetitions :: Int }
                  deriving (Show)
 
+noteEventToRhythm :: NoteEvent -> Rhythm
+noteEventToRhythm (Note _ rhythm)           = rhythm
+noteEventToRhythm (AccentedNote _ rhythm _) = rhythm
+noteEventToRhythm (Rest rhythm)             = rhythm
+
+simpleCanonToScore :: SimpleCanon -> Score
+simpleCanonToScore (SimpleCanon title (Motto notes) (Rhythm dist) instrument repetitions) =
+  midiVoicesScore title sections
+  where
+    dur = sum $ map (getRhythm . noteEventToRhythm) notes
+    voices = fromInteger $ numerator (dur / dist)
+    tune = concat $ replicate repetitions notes
+    rests = map (Rest . Rhythm) $ take voices [(0%1)*dist, (1%1)*dist..]
+    sections = [Section instrument [rest : tune] [] | rest <- rests]
+
+-- Frere Jacques
 mC = Pitch C 0
 mD = Pitch D 0
 mE = Pitch E 0
@@ -39,28 +53,14 @@ rh1 = [qtr, qtr, qtr, qtr]
 rh2 = [qtr, qtr, hlf]
 rh3 = [eig, eig, eig, eig, qtr, qtr]
 rhythms = rh1 ++ rh1 ++ rh2 ++ rh2 ++ rh3 ++ rh3 ++ rh2 ++ rh2
-ns = Motto $ zipWith Note pitches rhythms
-dist = Rhythm $ sum (map getRhythm $ rh1 ++ rh1)
-instr = Instrument "Acoustic Grand Piano"
-fj = SimpleCanon "Frere Jacques" ns dist instr 4 2
-
-noteEventToRhythm :: NoteEvent -> Rhythm
-noteEventToRhythm (Note _ rhythm)           = rhythm
-noteEventToRhythm (AccentedNote _ rhythm _) = rhythm
-noteEventToRhythm (Rest rhythm)             = rhythm
-  
-simpleCanonToScore :: SimpleCanon -> Score
-simpleCanonToScore (SimpleCanon title notes (Rhythm dist) instrument voices repetitions) =
-  midiVoicesScore title sections
-  where
-    tune = concat $ replicate repetitions $ getMotto notes
-    rests = map (Rest . Rhythm) $ take voices [(0%1)*dist, (1%1)*dist..]
-    sections = [Section instrument [rest : tune] [] | rest <- rests]
 
 writeFJCanon :: IO ()
 writeFJCanon =
-  LazyByteString.writeFile (canonTitle ++ ".mid") (SaveFile.toByteString (scoreToMidiFile fjScore))
+  LazyByteString.writeFile (title ++ ".mid") $ SaveFile.toByteString (scoreToMidiFile $ simpleCanonToScore simpleCanon)
   where
-    sc = SimpleCanon "Frere Jacques" ns dist instr 4 5
-    canonTitle = title sc
-    fjScore = simpleCanonToScore sc
+    title = "Frere Jacques"
+    noteMotto = Motto $ zipWith Note pitches rhythms
+    distance = Rhythm $ sum (map getRhythm $ rh1 ++ rh1)
+    instrument = Instrument "Frere Jacques"
+    repetitions = 5
+    simpleCanon = SimpleCanon title noteMotto distance instrument repetitions
